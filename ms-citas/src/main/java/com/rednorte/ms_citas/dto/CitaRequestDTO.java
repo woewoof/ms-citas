1ee1773e1d63e03ae2890c784aefebd8e04fba0a
woeewoof <camila.malhue05@gmail.com>
2026-06-10 21:57:35 -0400

    citasrequestdto

--- ms-citas/src/main/java/com/rednorte/ms_citas/dto/CitaRequestDTO.java
+++ ms-citas/src/main/java/com/rednorte/ms_citas/dto/CitaRequestDTO.java
@@ -2,16 +2,31 @@ package com.rednorte.ms_citas.dto;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import lombok.*;
-
-import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Future;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CitaRequestDTO {
-    @NotNull(message = "El paciente es obligatorio")
+    @NotNull
     private Long pacienteId;
+
+    @NotNull
+    @Future(message = "La fecha debe ser futura")
+    private LocalDate fecha;
+
+    @NotNull
+    private LocalTime hora;
+
+    @NotBlank
+    private String nombreMedico;
+
+    @NotBlank
+    private String especialidad;
+
+    private String centroSalud;
+    private String motivo;
 }
